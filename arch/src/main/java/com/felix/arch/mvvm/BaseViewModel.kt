d9eccdd4f4cf453769edc67fe47c12d4dbbc43ee
黄明发 <mingfahuang@sfmail.sf-express.com>
2021-02-20 12:40:43 +0800

    【修改】baseViewModule添加默认的scope

--- arch/src/main/java/com/felix/arch/mvvm/BaseViewModel.kt
+++ arch/src/main/java/com/felix/arch/mvvm/BaseViewModel.kt
@@ -1,8 +1,18 @@
 package com.felix.arch.mvvm
 
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ViewModel
 import com.felix.utils.utils.ITAG
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 
 open class BaseViewModel : ViewModel(), LifecycleObserver, ITAG {
+    val scope = MainScope()
+
+    @OnLifecycleEvent(value = Lifecycle.Event.ON_DESTROY)
+    fun destory() {
+        scope.cancel()
+    }
 }
